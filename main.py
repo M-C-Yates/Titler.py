7930d32f9215d6c68fae045d8e7b00d156e33366
Matthew Yates <Mat.C.Yates@gmail.com>
2019-10-09 13:25:48 -0400

    added cli arg for amount of names to be generated

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ import random
 
 parser = argparse.ArgumentParser(description='a name generator')
 
-parser.add_argument("-d", default='\u0020', type=str,
+parser.add_argument("-d", default='\u0020',
                     choices=["\u0020", "\u2010", "\u005f"],
-                    help="string delimiter, default: '\u0020', options: '\u0020', '\u2010', '\u005f'", required=False)
-args = parser.parse_args()
+                    help="string delimiter, default: '\u0020'",
+                    required=False, type=str,)
 
+parser.add_argument(
+    '-a', default=1, help="amount of names to generate. default: 1", required=False, type=int)
+
+args = parser.parse_args()
 
 with open('words.json', 'r') as inFile:
     words = json.load(inFile)
 delimiter = args.d
-amount = 5
+amount = args.a
 
 for x in range(1, amount + 1):
     randAdjective = random.choice(words["adjectives"])
