56e6d8dcb1f48f57e079a5412c72b51abfcdeafa
Matthew Yates <Mat.C.Yates@gmail.com>
2019-10-09 13:20:58 -0400

    added choices for delimter

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import random
 parser = argparse.ArgumentParser(description='a name generator')
 
 parser.add_argument("-d", default='\u0020', type=str,
-                    help="string delimiter, default: '\u0020', options: '\u0020', '\u2010', '\u005f'")
+                    choices=["\u0020", "\u2010", "\u005f"],
+                    help="string delimiter, default: '\u0020', options: '\u0020', '\u2010', '\u005f'", required=False)
 args = parser.parse_args()
 
 with open('words.json', 'r') as inFile:
