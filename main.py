7cc9922f4d919fd3065dc751dc848ce80230248d
Matthew Yates <Mat.C.Yates@gmail.com>
2019-10-09 13:21:31 -0400

    added choices for delimiter

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ parser.add_argument("-d", default='\u0020', type=str,
                     help="string delimiter, default: '\u0020', options: '\u0020', '\u2010', '\u005f'", required=False)
 args = parser.parse_args()
 
+
 with open('words.json', 'r') as inFile:
     words = json.load(inFile)
 delimiter = args.d
